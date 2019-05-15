514b8ffaaef2c5cad0b9395fd89712d5c87ba030
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-05-15 10:44:44 +0100

    use o spike to create/serve index.html page for app

---
+++ src/note-list-view.js
@@ -0,0 +1,17 @@
+(function(exports) {
+  function NoteListView(notelist = new NoteList()) {
+    this.notelist = notelist;
+  };
+
+
+  NoteListView.prototype.printHTML = function(){
+    var html = "<ul>"
+    this.notelist.seeList().forEach(function(note) {
+      html += "<li><div>" + note + "</div></li>"
+    })
+    html += "</ul>"
+    return html
+  };
+
+  exports.NoteListView = NoteListView;
+})(this);
---
+++ tests/note-list-view-test.js
@@ -0,0 +1,17 @@
+(function(exports) {
+  function testReturnsHTML() {
+    var notelistview = new NoteListView();
+    notelistview.notelist.addNote("1");
+    notelistview.notelist.addNote("2");
+    var htmlString = "<ul><li><div>1</div></li><li><div>2</div></li></ul>"
+
+    if (notelistview.printHTML() !== htmlString) {
+      throw new Error("HTML is not correct");
+    }
+    else {
+      console.log("testReturnsHTML: Clear")
+    }
+  };
+
+  testReturnsHTML();
+})(this);
